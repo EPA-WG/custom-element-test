67beb4cb35bd48f59592c9f941efde10d6eee682
Sasha Firsov <suns@firsov.net>
2023-03-04 21:43:51 -0800

    http-request.test.js passed

--- test/http-request.test.js
+++ test/http-request.test.js
@@ -7,13 +7,15 @@ import defaults, {HttpRequestSimple} from "../stories/http-request.stories";
 const defs = {}; Object.keys(defaults.argTypes).map( k=> defs[k]= defaults.argTypes[k].defaultValue);
 const renderStory = async (story) => fixture( story({ ...defs, ...story.args }) );
 
-describe.skip('http-request', () => {
-
-  it('http-request init', async () => {
-    const el = await renderStory(HttpRequestSimple);
-    expect(el.querySelectorAll('hr-test-component').length).to.equal(1);
-    const p = el.querySelector('hr-test-component');
-    expect(p.innerHTML).to.include('fruits slice: 🍒 from localStorage');
+describe('http-request', () =>
+{
+    it('http-request init', async () =>
+    {
+        const el = await renderStory(HttpRequestSimple);
+        expect(el.querySelectorAll('hr-test-component').length).to.equal(1);
+        await aTimeout(200);
+        expect(el.querySelectorAll('hr-test-component button').length).to.equal(6);
+        expect(el.querySelector('hr-test-component button').innerText).to.equal('bulbasaur');
   });
 
 });
